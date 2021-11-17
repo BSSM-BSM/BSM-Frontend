0d6c6e4fd1a73e185a5be9cc46853387c78e2274
SW2125\dltjd <leehj050211@gmail.com>
2021-11-17 21:59:28 +0900

    NodeJS 시간표 추가

--- public/js/search.js
+++ public/js/search.js
@@ -1,58 +1,58 @@
 function search(){
   resultCnt=0;
   $.ajax({
-    type:'POST',
-    data:{
-      command_type:'search',
-      searchType:'board',
-      searchQuery:$('.searchQuery').val()
-    },
-    url:'/database',
+    type:'GET',
+    url:apiUrl+'/search/board/'+$('.searchQuery').val(),
     cache:false,
     success:function(data){
       data=JSON.parse(data);
-      searchResults = "";
-      if(Object.keys(data).length==0){
-        $('.searchResult .boardResult').html("<h2>커뮤니티</h2><br>"+"검색 결과가 없습니다.<br>단어사이에 띄어쓰기를 하거나 검색어를 조금더 길게 적어주세요");
+      if(data.status!=1){
+          error_code(data.status, data.subStatus);
       }else{
-        for(var i=1;i<=Object.keys(data).length;i++){
-          var searchResult = "";
-          searchResult += "<li>"+data[i].postNo+"</li>";
-          searchResult += '<li><a href="/board/board/'+data[i].postNo+'"><p>'+data[i].postTitle+'</p></a></li>';
-          searchResult += "<li>"+data[i].memberNickname+"</li>";
-          searchResult += "<li>"+data[i].postDate+"</li>";
-          searchResults += "<ul>"+searchResult+"</ul>";
-          resultCnt++;
+        searchResults = "";
+        arrSearchResult = data.arrSearchResult;
+        if(Object.keys(arrSearchResult).length==0){
+          $('.searchResult .boardResult').html("<h2>자유게시판</h2><br>"+"검색 결과가 없습니다.<br>단어사이에 띄어쓰기를 하거나 검색어를 조금더 길게 적어주세요");
+        }else{
+          for(var i=1;i<=Object.keys(data).length;i++){
+            var searchResult = "";
+            searchResult += "<li>"+arrSearchResult[i].postNo+"</li>";
+            searchResult += '<li><a href="/board/board/'+arrSearchResult[i].postNo+'"><p>'+arrSearchResult[i].postTitle+'</p></a></li>';
+            searchResult += "<li>"+arrSearchResult[i].memberNickname+"</li>";
+            searchResult += "<li>"+arrSearchResult[i].postDate+"</li>";
+            searchResults += "<ul>"+searchResult+"</ul>";
+            resultCnt++;
+          }
+          $('.searchResult .boardResult').html("<h2>자유게시판</h2><br>"+searchResults);
         }
-        $('.searchResult .boardResult').html("<h2>커뮤니티</h2><br>"+searchResults);
       }
     }
   });
   $.ajax({
-    type:'POST',
-    data:{
-      command_type:'search',
-      searchType:'anonymous',
-      searchQuery:$('.searchQuery').val()
-    },
-    url:'/database',
+    type:'GET',
+    url:apiUrl+'/search/anonymous/'+$('.searchQuery').val(),
     cache:false,
     success:function(data){
       data=JSON.parse(data);
-      searchResults = "";
-      if(Object.keys(data).length==0){
-        $('.searchResult .blogResult').html("<h2>익명게시판</h2><br>"+"검색 결과가 없습니다.<br>단어사이에 띄어쓰기를 하거나 검색어를 조금더 길게 적어주세요");
+      if(data.status!=1){
+          error_code(data.status, data.subStatus);
       }else{
-        for(var i=1;i<=Object.keys(data).length;i++){
-          var searchResult = "";
-          searchResult += "<li>"+data[i].postNo+"</li>";
-          searchResult += '<li><a href="/board/anonymous/'+data[i].postNo+'"><p>'+data[i].postTitle+'</p></a></li>';
-          searchResult += "<li>"+data[i].memberNickname+"</li>";
-          searchResult += "<li>"+data[i].postDate+"</li>";
-          searchResults += "<ul>"+searchResult+"</ul>";
-          resultCnt++;
+        searchResults = "";
+        arrSearchResult = data.arrSearchResult;
+        if(Object.keys(arrSearchResult).length==0){
+          $('.searchResult .anonymousResult').html("<h2>익명게시판</h2><br>"+"검색 결과가 없습니다.<br>단어사이에 띄어쓰기를 하거나 검색어를 조금더 길게 적어주세요");
+        }else{
+          for(var i=1;i<=Object.keys(data).length;i++){
+            var searchResult = "";
+            searchResult += "<li>"+arrSearchResult[i].postNo+"</li>";
+            searchResult += '<li><a href="/board/board/'+arrSearchResult[i].postNo+'"><p>'+arrSearchResult[i].postTitle+'</p></a></li>';
+            searchResult += "<li>"+arrSearchResult[i].memberNickname+"</li>";
+            searchResult += "<li>"+arrSearchResult[i].postDate+"</li>";
+            searchResults += "<ul>"+searchResult+"</ul>";
+            resultCnt++;
+          }
+          $('.searchResult .anonymousResult').html("<h2>익명게시판</h2><br>"+searchResults);
         }
-        $('.searchResult .blogResult').html("<h2>익명게시판</h2><br>"+searchResults);
       }
     }
   });
