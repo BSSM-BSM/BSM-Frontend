207528d7eeae93b53a21b51db19d84aff96f94dc
SW2125\dltjd <leehj050211@gmail.com>
2021-11-20 21:49:22 +0900

    서버 NodeJS로 완전 변경

--- public/js/menu_bar.js
+++ public/js/menu_bar.js
@@ -13,29 +13,38 @@ $(document).ready(function(){
     }
   });
   $(".all_menu").on("click",function(){
-    if ($(".side_menu").attr("class").indexOf("on") > 1) {
-      $(".all_menu").removeClass("on");
-      $(".side_menu").removeClass("on");
-      $(".dim").removeClass("on");
-    } else {
-      $(".all_menu").addClass("on");
-      $(".side_menu").addClass("on");
-      $(".dim").addClass("on");
-    }
+    $(".all_menu").toggleClass("on");
+    $(".side_menu").toggleClass("on");
+    $(".dim.menu_close").toggleClass("on");
   });
   $(".menu_close").on("click",function(){
     $(".all_menu").removeClass("on");
     $(".side_menu").removeClass("on");
+    $(".dim.menu_close").removeClass("on");
+  });
+  $(".search_close").on("click",function(){
     $(".searchResult").removeClass("on");
-    $(".dim").removeClass("on");
+    $(".dim.search_close").removeClass("on");
   });
   $(".searchBox").on("click",function(){
-    if ($(".searchResult").attr("class").indexOf("on") > 1) {
-      $(".searchResult").removeClass("on");
-      $(".dim").removeClass("on");
-    } else {
-      $(".searchResult").addClass("on");
-      $(".dim").addClass("on");
-    }
+    $(".searchResult").toggleClass("on");
+    $(".dim.search_close").toggleClass("on");
+  });
+  $(".popup_close").on("click",function(){
+    $(".popup").trigger('removeClass');
+    $(".dim.popup_close").removeClass("on");
+  });
+  $(".popup").on('addClass', function() {
+    $(this).addClass("on").trigger('classChange');
+  });
+  $(".popup").on('removeClass', function() {
+    $(this).removeClass("on").trigger('classChange');
+  });
+  $(".popup").on('classChange', function() {
+    if($(".popup").hasClass("on")===true){
+      $(".dim.popup_close").addClass("on")
+    }else{
+      $(".dim.popup_close").removeClass("on")
+    } 
   });
 });
