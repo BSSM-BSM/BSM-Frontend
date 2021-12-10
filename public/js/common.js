95776cf2c207db70d87c8a49494f40ca9b904828
leehj050211 <leehj050211@gmail.com>
2021-12-10 22:30:43 +0900

    Remove: 생일 페이지, Fix: 코드 리펙토링

---
+++ public/js/common.js
@@ -0,0 +1,18 @@
+const agent = navigator.userAgent.toLowerCase();
+if((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1)){
+    $$('.notice_bar')[0].innerHTML+='<div class="notice red">현재 사용하시는 브라우저는 정상적으로 지원되지 않습니다</div>';
+}
+if(navigator.platform.match(/i(Phone|Pod)/i)!=null){
+    $$('.notice_bar')[0].innerHTML+='<div class="notice yellow">IOS환경에서는 제대로 동작하지 않을 수 있습니다.</div>';
+}
+window.addEventListener('online', ()=>{
+    if($$('.notice_bar .offline').length){
+        $$('.notice_bar .offline')[0].remove()
+    }
+})
+window.addEventListener('offline', ()=>{
+    $$('.notice_bar')[0].innerHTML+='<div class="notice red offline">인터넷에 연결되어있지 않습니다.</div>'
+})
+if(!window.navigator.onLine){
+    $$('.notice_bar')[0].innerHTML+='<div class="notice red offline">인터넷에 연결되어있지 않습니다.</div>'
+}
