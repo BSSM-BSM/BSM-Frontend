a245b7034109ff896082781336f0584b156e381f
leehj050211 <leehj050211@gmail.com>
2021-12-14 21:56:48 +0900

    Add: 마이스터 역량인증제 점수 보기

---
+++ public/js/etc/meister.js
@@ -0,0 +1,68 @@
+let hak=member.grade, ban=member.classNo, bun=member.studentNo
+const meisterView = new Vue({
+    el:'.meister',
+    data:{
+        viewCase:'score'
+    }
+})
+const meister = () => {
+    switch(meisterView.viewCase){
+        case 'score':
+            meisterScore();
+            break;
+        case 'point':
+            meisterPoint();
+            break;
+    }
+}
+const meisterPoint = () => {
+    $.ajax({
+        type:'POST',
+        data:{
+            pw: $('.meisterInfo .pw').val(),
+        },
+        url:apiUrl+'/meister/point/'+
+        $('.meisterInfo .hak').val()+'/'+
+        $('.meisterInfo .ban').val()+'/'+
+        $('.meisterInfo .bun').val(),
+        cache:false,
+        success:data => {
+            $('.meisterInfo .pw').val("")
+            $('.meister .result').html(data)
+            $$('.fas.fa-sad-cry').forEach(item =>{
+                item.parentElement.parentElement.parentElement.parentElement.classList.add('bad')
+            })
+        },
+        error:() => {
+            error_code(0, 0);
+        }
+    });
+}
+const meisterScore = () => {
+    $.ajax({
+        type:'GET',
+        url:apiUrl+'/meister/score/'+
+        $('.meisterInfo .hak').val()+'/'+
+        $('.meisterInfo .ban').val()+'/'+
+        $('.meisterInfo .bun').val(),
+        cache:false,
+        success:data => {
+            data=JSON.parse(data);
+            if(data.status!=1){
+                if(data.status==3&&data.subStatus==8){
+                    showAlert('에러코드 3_8 학생정보가 맞지 않거나 불러올 수 없는 학생입니다.')
+                }else{
+                    error_code(data.status, data.subStatus);
+                }
+            }else{
+                $('.meister .result').html(data.result)
+            }
+        },
+        error:() => {
+            error_code(0, 0);
+        }
+    });
+}
+$('.meisterInfo .hak').val(hak)
+$('.meisterInfo .ban').val(ban)
+$('.meisterInfo .bun').val(bun)
