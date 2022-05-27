c5da9ea043ec63166c301f181e7e18b6b6bb752a
leehj050211 <leehj050211@gmail.com>
2022-05-27 21:58:28 +0900

    Fix: 푸시 알림 등록 버그

--- public/js/subscribe.js
+++ public/js/subscribe.js
@@ -4,14 +4,15 @@ const mealSubscribe = () => {
             serviceWorkerRegistration.pushManager.subscribe({
                 userVisibleOnly:true,
                 applicationServerKey:'BCeveCXwornm1DNHuNAYDpy8MadIa2-i9ykSG2AORLNbGDPo5QALgEiln_pDX71Pnq4O7UM0EJq_8KeMv_rolAU'
-            }).then(sub => {
+            }).then(data => {
+                const sub = data.toJSON();
                 ajax({
                     method: 'post',
                     url: '/push/meal',
                     payload: {
                         endpoint: sub.endpoint,
                         auth: sub.keys.auth,
-                        p256dh: sub.keys.p256dh,
+                        p256dh: sub.keys.p256dh
                     },
                     callback: () => {
                         showToast('급식 알림등록이 완료되었습니다.<br>알림은 급식 1시간 전에 도착합니다.');
